b7b0b8e2ebce6e8848aedb288dd2eb2f85c2fee1
Rishammhd <rishambinahammed@gmail.com>
2025-11-13 11:01:38 +0530

    Login page is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/pages/homescreen.dart';
 import 'package:to_do_app/pages/loginscreen.dart';
+import 'package:to_do_app/pages/signup.dart';
 
 void main() {
   runApp(ToDoApp());
@@ -12,7 +14,14 @@ class ToDoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.purple),
-      home: Loginscreen(),
+      // home: Loginscreen(),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => Loginscreen(),
+        "/loginscreen": (context) => Loginscreen(),
+        "/homescreen": (context) => Homescreen(),
+        "/signup": (context) => Signup(),
+      },
     );
   }
 }
--- lib/pages/loginscreen.dart
+++ lib/pages/loginscreen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/pages/homescreen.dart';
 
-class Loginscreen extends StatelessWidget {
+class Loginscreen extends StatefulWidget {
   const Loginscreen({super.key});
+
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  bool _showpassword = true;
+  final _formkey = GlobalKey<FormState>();
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,86 +20,153 @@ class Loginscreen extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Spacer(flex: 2),
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Spacer(flex: 2),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
 
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-              // username
-              Center(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: TextFormField(
-                    decoration: InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              // paswword
-              Center(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: TextFormField(
-                    decoration: InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-              // Login Button
-              Center(
-                child: ElevatedButton(
-                  onPressed: null,
-                  style: ButtonStyle(
-                    minimumSize: WidgetStatePropertyAll(
-                      Size(MediaQuery.of(context).size.width * 0.9, 50),
-                    ),
-                    backgroundColor: WidgetStatePropertyAll(
-                      Theme.of(context).primaryColor,
-                    ),
-                    shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
                   child: Text(
                     "Login",
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("No account ?"),
-                  TextButton(
-                    onPressed: null,
-                    child: Text(
-                      "SignUp",
-                      style: TextStyle(color: Theme.of(context).primaryColor),
+                SizedBox(height: 15),
+                // username
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: TextFormField(
+                      controller: _username,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your username or email";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Username | Email adress "),
+                      ),
                     ),
                   ),
-                ],
-              ),
-              const Spacer(flex: 3),
-            ],
+                ),
+                SizedBox(height: 10),
+                // paswword
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: TextFormField(
+                      controller: _password,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your password";
+                        }
+                        return null;
+                      },
+                      obscureText: _showpassword,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Password",
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _showpassword = !_showpassword;
+                            });
+                          },
+                          icon: Icon(
+                            _showpassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                // Login Button
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (_) => Homescreen()),
+                        );
+                      }
+                    },
+                    style: ButtonStyle(
+                      minimumSize: WidgetStatePropertyAll(
+                        Size(MediaQuery.of(context).size.width * 0.9, 50),
+                      ),
+                      backgroundColor: WidgetStatePropertyAll(
+                        Theme.of(context).primaryColor,
+                      ),
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // FORGOT YOUR PASSWORD BUTTON
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+
+                      child: Text(
+                        "Forgot your password",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // SIGNUP BUTTON
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("No account ?"),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, "/signup");
+                      },
+                      child: Text(
+                        "SignUp",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(flex: 3),
+              ],
+            ),
           ),
         ),
       ),
